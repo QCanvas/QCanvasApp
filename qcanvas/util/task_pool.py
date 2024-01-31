b0b9ea15c6b40306df4aabc7978299168c64d0cc
QCanvas <QCanvas@noreply.codeberg.org>
2024-01-31 20:31:05 +1100

    Add some more utility functions

--- qcanvas/util/task_pool.py
+++ qcanvas/util/task_pool.py
@@ -13,16 +13,17 @@ class TaskPool(Generic[T]):
     submitted again.
     """
 
-    _results: dict[object, asyncio.Event | T | None]
+    _results: dict[object, asyncio.Event | T | None] = {}
     _semaphore = asyncio.Semaphore()
 
     _remember_result: bool
     _wait_if_in_progress: bool
     _wait_if_just_started: bool
+    _restart_if_finished: bool
     _echo: bool
 
     def __init__(self, remember_result: bool = True, wait_if_in_progress: bool = True,
-                 wait_if_just_started: bool = True, echo: bool = False):
+                 wait_if_just_started: bool = True, restart_if_finished: bool = False, echo: bool = False):
         """
         Parameters
         ----------
@@ -46,8 +47,8 @@ class TaskPool(Generic[T]):
         self._remember_result = remember_result
         self._wait_if_in_progress = wait_if_in_progress
         self._wait_if_just_started = wait_if_just_started
+        self._restart_if_finished = restart_if_finished
         self._echo = echo
-        self._results = {}
 
     def add_completed_values(self, results: dict[object, T]) -> None:
         """
@@ -86,19 +87,26 @@ class TaskPool(Generic[T]):
             Extra arguments for the lambda
         Returns
         -------
-        None, if the configuration states that we should not wait for the function to finish or not to remember the result.
-        The result of the function otherwise.
+        T | None
+            None, if the configuration states that we should not wait for the function to finish or not to remember the result.
+            Returns the result of the function otherwise.
         """
         sem = self._semaphore
         await sem.acquire()
 
         if task_id in self._results.keys():
-            if not self._wait_if_in_progress:
-                if self._echo: print(f"Task {task_id} in progress or finished. Configured to not wait.")
+            if not self._wait_if_in_progress and isinstance(self._results[task_id], asyncio.Event):
+                if self._echo: print(f"Task {task_id} in progress but configured to not wait, returning None.")
                 sem.release()
                 return None
 
             if not isinstance(self._results[task_id], asyncio.Event):
+                if self._restart_if_finished:
+                    if self._echo: print(
+                        f"Task {task_id} already finished but configured to restart if finished, restarting.")
+                    # start_task releases the semaphore, no need to do it here
+                    return await self._start_task(task_id, func, **kwargs)
+
                 if self._echo: print(f"Task {task_id} already finished, returning.")
                 sem.release()
                 return self._results[task_id]
@@ -110,22 +118,45 @@ class TaskPool(Generic[T]):
 
             await event.wait()
 
-            if self._echo: print(f"Task {task_id} finished.")
+            if self._echo: print(f"Finished waiting for {task_id}.")
 
             return self._results[task_id]
         else:
-            if self._echo: print(f"Task {task_id} started - Origin.")
+            # start_task releases the semaphore, no need to do it here
+            return await self._start_task(task_id, func, **kwargs)
 
-            event = asyncio.Event()
-            self._results[task_id] = event
-            sem.release()
+    async def _start_task(self, task_id: object, func, **kwargs):
+        """
+        Starts a task and **releases the results list semaphore**
 
-            if not self._wait_if_just_started:
-                # noinspection PyAsyncCall
-                asyncio.create_task(self._handle_task(func(**kwargs), task_id, event))
-                return None
+        Parameters
+        ----------
+        task
+            The task to start
+        task_id
+            The ID of the task
+        event
+            The event that the task is attached to
 
-            return await self._handle_task(func(**kwargs), task_id, event)
+        Returns
+        -------
+        T
+            The result of the task
+        """
+        sem = self._semaphore
+
+        if self._echo: print(f"Task {task_id} started.")
+
+        event = asyncio.Event()
+        self._results[task_id] = event
+        sem.release()
+
+        if not self._wait_if_just_started:
+            # noinspection PyAsyncCall
+            asyncio.create_task(self._handle_task(func(**kwargs), task_id, event))
+            return None
+
+        return await self._handle_task(func(**kwargs), task_id, event)
 
     async def _handle_task(self, task: Awaitable[object], task_id: object, event: asyncio.Event) -> T:
         """
@@ -142,6 +173,7 @@ class TaskPool(Generic[T]):
 
         Returns
         -------
+        T
             The result of the task
         """
         sem = self._semaphore
@@ -153,7 +185,7 @@ class TaskPool(Generic[T]):
 
         await sem.acquire()
 
-        if self._echo: print(f"Task {task_id} finished - Origin.")
+        if self._echo: print(f"Task {task_id} finished.")
 
         self._results[task_id] = result if self._remember_result else None
         event.set()
@@ -161,18 +193,72 @@ class TaskPool(Generic[T]):
 
         return result
 
+    async def wait_if_in_progress(self, task_id: object):
+        """
+        Waits for a task if it is in progress. Returns immediately otherwise.
+
+        Parameters
+        ----------
+        task_id
+            The task id to wait for
+        """
+        sem = self._semaphore
+
+        await sem.acquire()
+
+        if task_id in self._results and isinstance(self._results[task_id], asyncio.Event):
+            event: asyncio.Event = self._results[task_id]
+            sem.release()
+
+            await event.wait()
+
     def clear(self) -> None:
         """
         Deletes the stored results
 
         Returns
         -------
-            None
+        None
         """
         self._results.clear()
 
-    def results(self) -> list[T | None]:
+    def results(self) -> list[T]:
+        """
+        Gets the results of all currently completed tasks
+
+        Returns
+        -------
+        list[T]
+            The results of all currently completed tasks, excluding Nones
+        """
         def filter_func(it):
             return not isinstance(it, asyncio.Event) and it is not None
 
         return list(filter(filter_func, self._results.values()))
+
+    def get_completed_result(self, task_id: object) -> T | None:
+        """
+        Returns the result of an already completed task
+
+        Returns
+        -------
+        object
+            The result of the task
+
+        Raises
+        ------
+        ValueError
+            If the task is still in progress
+
+        KeyError
+            If the task has not been started yet or does not exist
+        """
+        if task_id in self._results:
+            result = self._results[task_id]
+
+            if isinstance(result, asyncio.Event):
+                raise ValueError(f"{task_id} is still in progress")
+
+            return result
+        else:
+            raise KeyError(f"{task_id} has not been started")
