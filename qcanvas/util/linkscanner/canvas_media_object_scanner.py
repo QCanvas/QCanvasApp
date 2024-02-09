a2042a13d4bd926e15974edf02149ca6e91dedbb
QCanvas <QCanvas@noreply.codeberg.org>
2024-02-09 18:33:46 +1100

    Support indexing canvas embedded videos

---
+++ qcanvas/util/linkscanner/canvas_media_object_scanner.py
@@ -0,0 +1,66 @@
+import json
+from typing import Any
+
+from bs4 import Tag, BeautifulSoup
+from httpx import URL, AsyncClient
+
+from qcanvas import db as db
+from qcanvas.util.linkscanner import ResourceScanner
+
+
+class CanvasMediaObjectScanner(ResourceScanner):
+
+    def __init__(self, client : AsyncClient):
+        self.client = client
+
+    @property
+    def name(self) -> str:
+        return "canvas_media_object"
+
+    def accepts_link(self, link: Tag) -> bool:
+        return (
+                link.name == "iframe"
+                and "data-media-type" in link.attrs.keys()
+                and link.attrs["data-media-type"] == "video"
+        )
+
+    async def extract_resource(self, link: Tag) -> db.Resource:
+        # Get the page for the embedded player (I could not find another way to get the needed data from canvas)
+        response = (await self.client.get(link.attrs["src"])).text
+        # Parse the HTML response
+        doc = BeautifulSoup(response, "html.parser")
+        media_data: None | dict[str, Any] = None
+
+        # Find all script tags (one of them has the data we are interested in)
+        for script_tag in doc.find_all("script", {}):
+            body = script_tag.text.strip()
+
+            # If the tag content starts with this then it has the data we want
+            if "INST = {" in body:
+                # Find the data that we are interested in (is on a line that starts with "ENV = ")
+                line: str = next(filter(lambda x: x.strip().startswith("ENV ="), script_tag.text.splitlines()))
+                # Parse the json embedded in the script tag
+                media_data = json.loads(line.lstrip("ENV = ").rstrip(";"))["media_object"]
+                break
+
+        if media_data is None:
+            raise Exception("Could not extract media info")
+
+        # The highest quality stream is the first
+        media_source = media_data["media_sources"][0]
+
+        return db.Resource(
+            self.extract_id(link),
+            media_source["src"],
+            media_data["title"],
+            media_data["media_id"],
+            media_source["size"]
+        )
+
+    def extract_id(self, link: Tag) -> str:
+        url = URL(link.attrs["src"])
+
+        return "canvas_media_" + url.path.rsplit("/", 2)[-1]
+
+    async def download(self):
+        pass
