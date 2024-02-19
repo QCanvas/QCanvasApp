a2832296729a77ff71a0a08476b472a3bfa7e78f
QCanvas <QCanvas@noreply.codeberg.org>
2024-02-20 10:34:26 +1100

    Remove class instance variable

--- qcanvas/ui/container_item.py
+++ qcanvas/ui/container_item.py
@@ -5,12 +5,19 @@ from qcanvas.util import tree_util as tree
 
 
 class ContainerItem(QStandardItem):
-    content: tree.HasText
-
     def __init__(self, data: tree.HasText):
         super().__init__()
         self.content = data
 
+
     def data(self, role=257):
         if role == Qt.ItemDataRole.DisplayRole:
             return self.content.text
+
+    def setData(self, value, role=257):
+        print(value)
+
+
+
+
+
