b903eaf85589adebe52d05d7e626884522a71648
QCanvas <QCanvas@noreply.codeberg.org>
2024-02-20 16:25:14 +1100

    Hold app config in ram to protect from changes on disk

--- qcanvas/util/app_settings.py
+++ qcanvas/util/app_settings.py
@@ -2,22 +2,27 @@ from qcanvas.QtVersionHelper.QtCore import QSettings, QUrl
 
 
 class _AppSettings:
-    settings = QSettings("RetardSoft", "QCanvasViewer")
+    def __init__(self):
+        self.settings = QSettings("RetardSoft", "QCanvasViewer")
+        self._canvas_url = self.settings.value("canvas_url", None)
+        self._api_key = self.settings.value("api_key", defaultValue=None)
 
     @property
     def canvas_url(self) -> str | None:
-        return self.settings.value("canvas_url", None)
+        return self._canvas_url
 
     @canvas_url.setter
     def canvas_url(self, value: str):
+        self._canvas_url = value
         self.settings.setValue("canvas_url", value)
 
     @property
     def canvas_api_key(self) -> str | None:
-        return self.settings.value("api_key", defaultValue=None)
+        return self._api_key
 
     @canvas_api_key.setter
     def canvas_api_key(self, value: str):
+        self._api_key = value
         self.settings.setValue("api_key", value)
 
     @property
