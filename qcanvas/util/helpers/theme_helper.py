df2cc15b6874931b5a940fc12ef0a2763929ac4f
QCanvas <QCanvas@noreply.codeberg.org>
2024-02-22 23:31:23 +1100

    Add some cool colours i guess

--- qcanvas/util/helpers/theme_helper.py
+++ qcanvas/util/helpers/theme_helper.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import qdarktheme
 
 from qcanvas.util.app_settings import settings
@@ -10,5 +12,34 @@ def apply_selected_theme() -> None:
     if settings.theme != "native":
         qdarktheme.setup_theme(
             settings.theme,
-            custom_colors={"primary": "FF804F"}
+            custom_colors=_get_colours()
         )
+
+
+red_theme = {
+    "primary": "e21d31",
+    "[light]": {"foreground": "480910", "background": "fcf8f8"},
+    "[dark]": {"foreground": "fbdfe2", "background": "231f1f"}
+}
+
+
+def _get_colours() -> dict:
+    now = datetime.now()
+
+    if now.year >= 2025:
+        print("I certainly hope not")
+
+    if now.month == 3 and now.day == 17:
+        # And this is on the weekend...
+        return {"primary": "08ff00"}
+    elif now.month == 2 and now.day == 14:
+        print("Why are you looking at canvas? Don't you have something better to do?")
+
+        # Nobody will ever see this because uni starts around the 20th
+        # Too bad, I kinda liked the theme
+        return red_theme
+    elif now.month == 8 and now.day == 20:
+        # Some random day... I just wanted to see the red theme
+        return red_theme
+    else:
+        return {"primary": "FF804F"}
