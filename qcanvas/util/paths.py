d6094672f329d7b102d759da0c6255a90d63b3a3
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-17 13:37:14 +1000

    Add data storage directory

--- qcanvas/util/paths.py
+++ qcanvas/util/paths.py
@@ -4,9 +4,13 @@ from pathlib import Path
 _logger = logging.getLogger(__name__)
 
 
-def root():
+def root() -> Path:
     return Path()
 
 
-def ui_storage():
+def ui_storage() -> Path:
     return root() / ".ui"
+
+
+def data_storage() -> Path:
+    return root() / ".data"
