dbff6a5966390070fea385c20f99ed1370f45387
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-18 22:10:25 +1000

    Replace nbsp entity instead of literal nbsp, remove fontawesome icons

--- qcanvas/util/html_cleaner.py
+++ qcanvas/util/html_cleaner.py
@@ -1,20 +1,25 @@
 import logging
 import re
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 
 _logger = logging.getLogger(__name__)
-
-NBSP = "\N{NO-BREAK SPACE}"
 
 
 def clean_up_html(html: str) -> str:
     html = re.sub(r"background-color:\s?initial;?", "", html)
-    html = html.replace(NBSP, " ")
+    html = html.replace("&nbsp;", " ")
 
     doc = BeautifulSoup(html, "html.parser")
 
-    for bad_tag in doc.find_all(["link", "script"]):
-        bad_tag.decompose()
+    # Remove all scripts and css
+    _remove_tags(doc.find_all(["link", "script"]))
+    # Remove font awesome icons (which don't load anyway)
+    _remove_tags(doc.find_all(["span"], class_=["dp-icon-content"]))
 
     return str(doc)
+
+
+def _remove_tags(tags: ResultSet) -> None:
+    for tag in tags:
+        tag.decompose()
