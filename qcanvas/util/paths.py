6e73b70c3b52ab795f7e120b51c9d66df20c1f24
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-18 10:32:55 +1000

    Use a fixed directory instead of the current one for data in release build

--- qcanvas/util/paths.py
+++ qcanvas/util/paths.py
@@ -1,11 +1,37 @@
 import logging
+import sys
 from pathlib import Path
+from sys import platform
+
+from PySide6.QtCore import QStandardPaths
+from qtpy.QtCore import QDir
+from qtpy.QtCore import QSettings
 
 _logger = logging.getLogger(__name__)
 
+_is_running_on_windows = platform.system() == "Windows"
+_is_running_as_pyinstaller = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+
+
+def client_settings() -> QSettings:
+    if _is_running_as_pyinstaller and _is_running_on_windows:
+        return QSettings(
+            str(Path(QDir.homePath()) / ".config" / "QCanvasTeam" / "canvas.ini"),
+            QSettings.Format.IniFormat,
+        )
+    else:
+        return QSettings("QCanvasTeam", "QCanvas")
+
 
 def root() -> Path:
-    return Path()
+    if _is_running_as_pyinstaller:
+        return Path(
+            QStandardPaths.standardLocations(
+                QStandardPaths.StandardLocation.AppLocalDataLocation
+            )[0]
+        )
+    else:
+        return Path()
 
 
 def ui_storage() -> Path:
--- qcanvas/util/settings/client_settings.py
+++ qcanvas/util/settings/client_settings.py
@@ -1,30 +1,17 @@
 import logging
-import platform
-import sys
-from pathlib import Path
 from typing import *
 
 from qcanvas_api_clients.canvas import CanvasClientConfig
 from qcanvas_api_clients.panopto import PanoptoClientConfig
-from qtpy.QtCore import QDir, QSettings
 
+from qcanvas.util import paths
 from qcanvas.util.settings.mapped_setting import MappedSetting
 
 _logger = logging.getLogger(__name__)
 
-_is_running_on_windows = platform.system() == "Windows"
-_is_running_as_pyinstaller = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
-
 
 class _ClientSettings:
-    if _is_running_as_pyinstaller and _is_running_on_windows:
-        settings = QSettings(
-            str(Path(QDir.homePath()) / ".config" / "QCanvasTeam" / "canvas.ini"),
-            QSettings.Format.IniFormat,
-        )
-    else:
-        settings = QSettings("QCanvasTeam", "QCanvas")
-
+    settings = paths.client_settings()
     canvas_url: MappedSetting[Optional[str]] = MappedSetting(default=None)
     canvas_api_key: MappedSetting[Optional[str]] = MappedSetting(default=None)
     panopto_url: MappedSetting[Optional[str]] = MappedSetting(default=None)
