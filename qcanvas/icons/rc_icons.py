d584256b8480949ab342c39ac035d71c1b3f8ecd
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-20 11:14:47 +1000

    Replace references to pyside

--- qcanvas/icons/rc_icons.py
+++ qcanvas/icons/rc_icons.py
@@ -3,7 +3,7 @@
 # Created by: The Resource Compiler for Qt version 6.7.2
 # WARNING! All changes made in this file will be lost!
 
-from PySide6 import QtCore
+from qtpy import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x04;\
@@ -545,10 +545,17 @@ qt_resource_struct = b"\
 \x00\x00\x01\x90\xcd\x99\xe4\x05\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
