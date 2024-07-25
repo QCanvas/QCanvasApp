055c3d465510e8c922fa3f1340a97bd011241d30
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-25 16:29:20 +1000

    Improve url validation

--- qcanvas/util/url_checker.py
+++ qcanvas/util/url_checker.py
@@ -1,13 +1,9 @@
 import logging
-from urllib.parse import urlparse
+
+import validators
 
 _logger = logging.getLogger(__name__)
 
 
 def is_url(url: str) -> bool:
-    # https://overflow.perennialte.ch/questions/7160737/how-to-validate-a-url-in-python-malformed-or-not#
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
+    return validators.url(url) == True
