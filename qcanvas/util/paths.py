6821b3e40ff6c88bd5199a4b3aae1465e8b706c2
QCanvas <QCanvas@noreply.codeberg.org>
2024-07-30 13:42:50 +1000

    Add portable install option

--- qcanvas/util/paths.py
+++ qcanvas/util/paths.py
@@ -10,6 +10,7 @@ from qtpy.QtCore import QSettings
 
 _logger = logging.getLogger(__name__)
 
+_is_running_portable = Path(".portable").exists()
 _is_running_on_windows = platform.system() == "Windows"
 _is_running_on_linux = platform.system() == "Linux"
 _is_running_as_pyinstaller = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
@@ -17,7 +18,9 @@ _is_running_as_flatpak = os.environ.get("container", "") == "flatpak"
 
 
 def client_settings() -> QSettings:
-    if _is_running_as_pyinstaller and _is_running_on_windows:
+    if _is_running_portable:
+        return QSettings("QCanvas.ini", QSettings.Format.IniFormat)
+    elif _is_running_as_pyinstaller and _is_running_on_windows:
         return QSettings(
             str(platformdirs.user_documents_path() / "QCanvasTeam" / "QCanvas.ini"),
             QSettings.Format.IniFormat,
@@ -30,10 +33,11 @@ def client_settings() -> QSettings:
 def root() -> Path:
     root_path = Path()
 
-    if _is_running_as_flatpak:
-        root_path = Path(os.environ["XDG_DATA_HOME"])
-    elif _is_running_as_pyinstaller:
-        root_path = platformdirs.user_data_path("QCanvasReborn", "QCanvasTeam")
+    if not _is_running_portable:
+        if _is_running_as_flatpak:
+            root_path = Path(os.environ["XDG_DATA_HOME"])
+        elif _is_running_as_pyinstaller:
+            root_path = platformdirs.user_data_path("QCanvasReborn", "QCanvasTeam")
 
     print("Root path", root_path.absolute())
     _logger.debug("Root path %s", root_path.absolute())
