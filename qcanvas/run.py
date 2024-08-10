366137b44d2a64d10c83f563931aac8788e8073d
QCanvas <QCanvas@noreply.codeberg.org>
2024-08-10 12:17:12 +1000

    Remove debug log level for themes util

--- qcanvas/run.py
+++ qcanvas/run.py
@@ -23,7 +23,7 @@
 # nuitka-project: --nofollow-import-to=yt_dlp.extractor.lazy_extractors
 
 import logging
-from logging import DEBUG, INFO, WARNING
+from logging import INFO, WARNING
 
 import qcanvas.app_start
 from qcanvas.util import logs, paths
@@ -42,7 +42,6 @@ logs.set_levels(
         "qcanvas.ui": WARNING,
         "qcanvas_backend": INFO,
         "qcanvas.ui.main_ui.status_bar_progress_display": INFO,
-        "qcanvas.util.themes": DEBUG,
     }
 )
 
