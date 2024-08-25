99a051293280f77f4d04a0f69e488d61c253c2a9
QCanvas <QCanvas@noreply.codeberg.org>
2024-08-25 15:29:50 +1000

    Add dark mode icons

--- qcanvas/icons/__init__.py
+++ qcanvas/icons/__init__.py
@@ -7,45 +7,45 @@ from .rc_icons import (
 
 
 # noinspection PyPep8Naming
-class branding:
-    main_icon = QIcon.fromTheme("branding/main_icon")
-    logo_transparent = QIcon.fromTheme("branding/logo_transparent")
+class actions:
+    exit = QIcon.fromTheme("actions/exit")
+    mark_all_read = QIcon.fromTheme("actions/mark_all_read")
+    open_downloads = QIcon.fromTheme("actions/open_downloads")
+    quick_login = QIcon.fromTheme("actions/quick_login")
+    sync = QIcon.fromTheme("actions/sync")
 
 
 # noinspection PyPep8Naming
-class tree_items:
-    semester = QIcon.fromTheme("tree_items/semester")
-    page = QIcon.fromTheme("tree_items/page")
-    mail = QIcon.fromTheme("tree_items/mail")
-    module = QIcon.fromTheme("tree_items/module")
-    assignment = QIcon.fromTheme("tree_items/assignment")
+class branding:
+    logo_transparent = QIcon.fromTheme("branding/logo_transparent")
+    main_icon = QIcon.fromTheme("branding/main_icon")
 
 
 # noinspection PyPep8Naming
 class options:
-    include_videos = QIcon.fromTheme("options/include_videos")
-    ignore_old = QIcon.fromTheme("options/ignore_old")
     auto_download = QIcon.fromTheme("options/auto_download")
+    ignore_old = QIcon.fromTheme("options/ignore_old")
+    include_videos = QIcon.fromTheme("options/include_videos")
     theme = QIcon.fromTheme("options/theme")
 
 
 # noinspection PyPep8Naming
 class tabs:
+    assignments = QIcon.fromTheme("tabs/assignments")
     assignments_new_content = QIcon.fromTheme("tabs/assignments_new_content")
     mail = QIcon.fromTheme("tabs/mail")
-    pages_new_content = QIcon.fromTheme("tabs/pages_new_content")
-    pages = QIcon.fromTheme("tabs/pages")
-    assignments = QIcon.fromTheme("tabs/assignments")
     mail_new_content = QIcon.fromTheme("tabs/mail_new_content")
+    pages = QIcon.fromTheme("tabs/pages")
+    pages_new_content = QIcon.fromTheme("tabs/pages_new_content")
 
 
 # noinspection PyPep8Naming
-class actions:
-    exit = QIcon.fromTheme("actions/exit")
-    open_downloads = QIcon.fromTheme("actions/open_downloads")
-    sync = QIcon.fromTheme("actions/sync")
-    mark_all_read = QIcon.fromTheme("actions/mark_all_read")
-    quick_login = QIcon.fromTheme("actions/quick_login")
+class tree_items:
+    assignment = QIcon.fromTheme("tree_items/assignment")
+    mail = QIcon.fromTheme("tree_items/mail")
+    module = QIcon.fromTheme("tree_items/module")
+    page = QIcon.fromTheme("tree_items/page")
+    semester = QIcon.fromTheme("tree_items/semester")
 
 
 # noinspection PyPep8Naming
