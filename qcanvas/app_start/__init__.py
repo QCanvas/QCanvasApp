d9ad2f83861a4d5f55af16c5450f79b14ce9a10d
QCanvas <QCanvas@noreply.codeberg.org>
2024-10-07 18:49:47 +1100

    Removed qcanvas initialisation from the main window constructor

--- qcanvas/app_start/__init__.py
+++ qcanvas/app_start/__init__.py
@@ -4,13 +4,15 @@ import sys
 
 import qtpy
 from qasync import QEventLoop
+from qcanvas_backend.qcanvas import QCanvas
 from qtpy.QtGui import QGuiApplication
 from qtpy.QtWidgets import QApplication
 
 import qcanvas.backend_connectors.qcanvas_task_master as task_master
+from qcanvas.backend_connectors import FrontendResourceManager
 from qcanvas.ui.main_ui.qcanvas_window import QCanvasWindow
 from qcanvas.ui.setup import SetupDialog, setup_checker
-from qcanvas.util import runtime, settings, themes
+from qcanvas.util import paths, runtime, settings, themes
 
 main_window = None
 setup_window = None
@@ -20,8 +22,23 @@ _logger = logging.getLogger(__name__)
 
 def _show_main():
     global main_window
-    main_window = QCanvasWindow()
+    _qcanvas = asyncio.get_event_loop().run_until_complete(_setup_database())
+    main_window = QCanvasWindow(_qcanvas)
     main_window.show()
+
+
+async def _setup_database() -> QCanvas[FrontendResourceManager]:
+    _qcanvas = QCanvas[FrontendResourceManager](
+        canvas_config=settings.client.canvas_config,
+        panopto_config=settings.client.panopto_config,
+        storage_path=paths.data_storage(),
+        resource_manager_class=FrontendResourceManager,
+    )
+
+    await _qcanvas.database.upgrade()
+    await _qcanvas.init()
+
+    return _qcanvas
 
 
 def _show_qt_api_name():
@@ -45,15 +62,15 @@ def launch():
     event_loop = QEventLoop(app)
     asyncio.set_event_loop(event_loop)
 
-    app_close_event = asyncio.Event()
-    app.aboutToQuit.connect(app_close_event.set)
-
     if setup_checker.needs_setup():
         setup_window = SetupDialog()
         setup_window.show()
         setup_window.closed.connect(_show_main)
     else:
         _show_main()
+
+    app_close_event = asyncio.Event()
+    app.aboutToQuit.connect(app_close_event.set)
 
     with event_loop:
         event_loop.run_until_complete(app_close_event.wait())
