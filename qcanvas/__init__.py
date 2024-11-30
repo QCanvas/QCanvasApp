4fb0ff8856c75a08fc44b385f11bcdb832411603
QCanvas <QCanvas@noreply.codeberg.org>
2024-12-01 01:07:29 +1100

    Clean up app launcher

---
+++ qcanvas/__init__.py
@@ -0,0 +1,54 @@
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --windows-icon-from-ico=./deploy/windows/qcanvas.ico
+# nuitka-project: --windows-console-mode=attach
+# nuitka-project: --linux-icon=deploy/appimage/qcanvas.svg
+# nuitka-project: --standalone
+# nuitka-project: --include-module=yt_dlp
+
+# Anti-bloat
+
+##########################################################
+# nuitka-project: --include-package=aiosqlite
+# nuitka-project: --nofollow-import-to=aiosqlite.tests.*
+##########################################################
+# nuitka-project: --nofollow-import-to=rich
+##########################################################
+# nuitka-project: --nofollow-import-to=sqlalchemy.dialects.mssql
+# nuitka-project: --nofollow-import-to=sqlalchemy.dialects.postgresql
+# nuitka-project: --nofollow-import-to=sqlalchemy.dialects.oracle
+# nuitka-project: --nofollow-import-to=sqlalchemy.dialects.mysql
+##########################################################
+# nuitka-project: --nofollow-import-to=rich
+##########################################################
+# nuitka-project: --nofollow-import-to=yt_dlp.extractor.lazy_extractors
+
+import logging
+from logging import INFO, WARNING
+
+import qcanvas.app as app
+from qcanvas.util import logs, paths
+
+
+def main():
+    app.launch()
+
+
+if __name__ == "__main__":
+    paths.data_storage().mkdir(parents=True, exist_ok=True)
+
+    logging.basicConfig(
+        filemode="w",
+        filename=paths.data_storage() / "debug.log",
+        level="WARN",
+    )
+
+    logs.set_levels(
+        {
+            "qcanvas": INFO,
+            "qcanvas.ui": WARNING,
+            "qcanvas_backend": INFO,
+            "qcanvas.ui.main_ui.status_bar_progress_display": INFO,
+        }
+    )
+
+    main()
---
+++ qcanvas/app.py
@@ -0,0 +1,90 @@
+import asyncio
+import logging
+import sys
+
+import qtpy
+from qasync import QEventLoop, asyncSlot
+from qcanvas_backend.qcanvas import QCanvas
+from qtpy.QtCore import QObject, Signal, Slot
+from qtpy.QtGui import QGuiApplication
+from qtpy.QtWidgets import QApplication
+
+import qcanvas.backend_connectors.qcanvas_task_master as task_master
+from qcanvas.backend_connectors import FrontendResourceManager
+from qcanvas.ui.main_ui.qcanvas_window import QCanvasWindow
+from qcanvas.ui.setup import SetupDialog, setup_checker
+from qcanvas.util import paths, runtime, settings, themes
+
+_logger = logging.getLogger(__name__)
+
+
+# I couldn't figure out a reliable way of getting the event loop started.
+# This just uses a signal with an async slot to run some async functions and then shows the main window
+class _MainStarter(QObject):
+    _starting = Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._starting.connect(self._start)
+
+    @Slot()
+    def start(self):
+        self._starting.emit()
+
+    @asyncSlot()
+    async def _start(self):
+        _qcanvas = await self._setup_database()
+
+        _main_window = QCanvasWindow(_qcanvas)
+        _main_window.show()
+        self.setParent(_main_window)
+
+    async def _setup_database(self) -> QCanvas[FrontendResourceManager]:
+        _qcanvas = QCanvas[FrontendResourceManager](
+            canvas_config=settings.client.canvas_config,
+            panopto_config=settings.client.panopto_config,
+            storage_path=paths.data_storage(),
+            resource_manager_class=FrontendResourceManager,
+        )
+
+        await _qcanvas.database.upgrade()
+        await _qcanvas.init()
+
+        return _qcanvas
+
+
+def _show_qt_api_name():
+    print(f"Using Qt bindings from {qtpy.API_NAME}")
+    _logger.info("Using Qt bindings from %s", qtpy.API_NAME)
+
+
+def launch():
+    _show_qt_api_name()
+
+    app = QApplication(sys.argv)
+
+    if runtime.is_running_as_flatpak:
+        QGuiApplication.setDesktopFileName("io.github.qcanvas.QCanvasApp")
+
+    app.setApplicationName("QCanvas")
+
+    task_master.register()
+    themes.apply(settings.ui.theme)
+
+    event_loop = QEventLoop(app)
+    asyncio.set_event_loop(event_loop)
+
+    app_close_event = asyncio.Event()
+    app.aboutToQuit.connect(app_close_event.set)
+
+    _main = _MainStarter()
+
+    if setup_checker.needs_setup():
+        setup_window = SetupDialog()
+        setup_window.show()
+        setup_window.closed.connect(_main.start)
+    else:
+        _main.start()
+
+    with event_loop:
+        event_loop.run_until_complete(app_close_event.wait())
