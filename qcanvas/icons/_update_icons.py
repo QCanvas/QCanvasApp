e6c64b5ed6635f7484575809c242bfef6460c5c7
QCanvas <QCanvas@noreply.codeberg.org>
2024-12-18 17:05:35 +1100

    Improve icon manifest generator script

--- qcanvas/icons/_update_icons.py
+++ qcanvas/icons/_update_icons.py
@@ -2,11 +2,13 @@
 
 import glob
 import logging
-import os
 import subprocess
 from pathlib import Path
 
 import xmltodict
+import textwrap
+from mako.template import Template
+from collections import defaultdict
 
 _logger = logging.getLogger(__name__)
 
@@ -32,39 +34,37 @@ def generate_xml(all_icons: list[Path]) -> str:
 
 
 def generate_icon_defs(icons: list[Path]) -> str:
-    icons_base_removed = [remove_base_directory(icon) for icon in icons]
-    groups = group_icons_by_category(icons_base_removed)
-    classes = [
-        generate_group_class(group_name, icons) for group_name, icons in groups.items()
-    ]
+    groups = group_icons_by_category(icons)
 
-    return """from .rc_icons import qt_resource_data as _  # Without this, icon data will not be loaded
-from PySide6.QtGui import QIcon
-from ._icon_type import UniversalIcon, ThemeIcon
+    template = """
+    from .rc_icons import qt_resource_data as _  # Without this, icon data will not be loaded
+    from ._icon_type import UniversalIcon, ThemeIcon
+    from PySide6.QtGui import QIcon
+    
+    % for group_name, icons in groups.items():
+    # noinspection PyPep8Naming
+    class ${group_name}:
+        % for icon in sorted(icons):
+        ${icon.stem} = QIcon.fromTheme("${icon.parent / icon.stem}")
+        % endfor
+    
+    
+    % endfor      
+    """
 
-""" + "\n\n".join(classes)
+    template = Template(textwrap.dedent(template))
+    return template.render(groups=groups)
 
 
 def group_icons_by_category(icons: list[Path]) -> dict[str, set[Path]]:
-    groups: dict[str, set[Path]] = {}
+    groups: dict[str, set[Path]] = defaultdict(set)
 
     for icon in icons:
+        icon = remove_base_directory(icon)
         icon_category = icon.parts[0]
-        groups.setdefault(icon_category, set())
         groups[icon_category].add(icon)
 
     return groups
-
-
-def generate_group_class(group_name: str, icons: set[Path]) -> str:
-    # pascal_case_class_name = "".join([part.title() for part in group_name.split("_")])
-    lines = ["# noinspection PyPep8Naming", f"class {group_name}:"]
-
-    for icon in sorted(icons):
-        path_no_ext = os.path.splitext(icon)[0]
-        lines.append(f'    {icon.stem} = QIcon.fromTheme("{path_no_ext}")')
-
-    return "\n".join(lines)
 
 
 def run_rcc() -> None:
