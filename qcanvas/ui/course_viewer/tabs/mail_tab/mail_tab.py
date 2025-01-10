c75168cc0fe8e945309bad9841df27ee8637966c
QCanvas <QCanvas@noreply.codeberg.org>
2025-01-10 17:22:39 +1100

    Add attachments to announcements

--- qcanvas/ui/course_viewer/tabs/mail_tab/mail_tab.py
+++ qcanvas/ui/course_viewer/tabs/mail_tab/mail_tab.py
@@ -1,15 +1,18 @@
 import logging
+from typing import override
 
+from PySide6.QtCore import Qt
 from libqcanvas import db
-from libqcanvas.net.resources.download.resource_manager import ResourceManager
 from libqcanvas.net.sync.sync_receipt import SyncReceipt
 from libqcanvas.util import as_local
-from PySide6.QtWidgets import QLabel, QLayout
+from PySide6.QtWidgets import QLabel, QLayout, QMainWindow, QDockWidget
 
+from qcanvas.backend_connectors import FrontendResourceManager
 from qcanvas.ui.course_viewer.tabs.content_tab import ContentTab
 from qcanvas.ui.course_viewer.tabs.mail_tab.mail_tree import MailTree
 from qcanvas.ui.course_viewer.tabs.constants import date_strftime_format
 import qcanvas.util.ui_tools as ui
+from qcanvas.ui.qml_components import AttachmentsPane
 
 _logger = logging.getLogger(__name__)
 
@@ -21,18 +24,40 @@ class MailTab(ContentTab):
         *,
         course: db.Course,
         sync_receipt: SyncReceipt,
-        downloader: ResourceManager,
+        downloader: FrontendResourceManager,
     ):
+        self._main_container = QMainWindow()
+
         super().__init__(
             explorer=MailTree.create_from_receipt(course, sync_receipt=sync_receipt),
             title_placeholder_text="No mail selected",
             downloader=downloader,
         )
 
+        self._main_container.setCentralWidget(self._viewer)
+        self._files_pane = AttachmentsPane(downloader)
+        self._files_dock = ui.dock_widget(
+            widget=self._files_pane,
+            title="Attachments",
+            name="attachments",
+            min_size=ui.size(150, 100),
+            features=QDockWidget.DockWidgetFeature.DockWidgetMovable,
+        )
+        self._main_container.addDockWidget(
+            Qt.DockWidgetArea.TopDockWidgetArea, self._files_dock
+        )
         self._date_sent_label = QLabel("")
         self._sender_label = QLabel("")
 
         self.enable_info_grid()
+
+    @override
+    def _setup_layout(self) -> None:
+        super()._setup_layout()
+        self.content_grid.replaceWidget(
+            self._viewer,
+            self._main_container,
+        )
 
     def setup_info_grid(self) -> QLayout:
         return ui.form_layout(
@@ -44,3 +69,14 @@ class MailTab(ContentTab):
             as_local(mail.creation_date).strftime(date_strftime_format)
         )
         self._sender_label.setText(mail.sender_name)
+
+        if mail.attachments:
+            self._files_pane.load_files(mail.attachments)
+            self._files_dock.show()
+        else:
+            self._files_dock.hide()
+
+    @override
+    def _show_blank(self) -> None:
+        super()._show_blank()
+        self._files_dock.hide()
